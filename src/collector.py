c084301d00e725cfe45d06f4e7b068415093714f
Faton Haxhiu <fatonhaxhiu1@gmail.com>
2025-06-28 11:55:04 -0400

    Add bash history + SSH collection, GPG encryption option, verbose logging

--- src/collector.py
+++ src/collector.py
@@ -10,21 +10,29 @@ def parse_args():
     parser = argparse.ArgumentParser(description="Linux Forensics + Incident Response Toolkit")
     parser.add_argument('--output', '-o', default=None, help="Output directory (default: timestamped)")
     parser.add_argument('--package', '-p', action='store_true', help="Package artifacts into a tar.gz archive")
+    parser.add_argument('--encrypt', action='store_true', help="Encrypt archive with GPG (requires --package)")
     parser.add_argument('--verbose', '-v', action='store_true', help="Enable verbose output")
     return parser.parse_args()
+
+
+def log(message, verbose):
+    if verbose:
+        print(message)
 
 
 ARTIFACT_DIR = f"artifacts_{datetime.now().strftime('%Y%m%d_%H%M%S')}"
 os.makedirs(ARTIFACT_DIR, exist_ok=True)
 
 
-def save_command_output(cmd, outfile):
+def save_command_output(cmd, outfile, verbose=False):
+    log(f"Running command: {' '.join(cmd)} -> {outfile}", verbose)
     with open(os.path.join(ARTIFACT_DIR, outfile), 'w') as f:
         subprocess.run(cmd, stdout=f, stderr=subprocess.DEVNULL, text=True)
 
 
-def save_file_copy(src, dest):
+def save_file_copy(src, dest, verbose=False):
     if os.path.exists(src):
+        log(f"Copying file: {src} -> {dest}", verbose)
         with open(src, 'r') as f_src, open(os.path.join(ARTIFACT_DIR, dest), 'w') as f_dst:
             f_dst.write(f_src.read())
 
@@ -37,28 +45,44 @@ def compute_hash(filepath):
     return sha256.hexdigest()
 
 
-def package_artifacts():
+def package_artifacts(encrypt=False):
     archive_name = ARTIFACT_DIR + '.tar.gz'
     with tarfile.open(archive_name, 'w:gz') as tar:
         tar.add(ARTIFACT_DIR)
-    print("Artifacts packaged into {}".format(archive_name))
+    print(f"Artifacts packaged into {archive_name}")
+
+    if encrypt:
+        enc_name = archive_name + '.gpg'
+        subprocess.run(['gpg', '--symmetric', '--cipher-algo', 'AES256', '-o', enc_name, archive_name])
+        print(f"Encrypted archive created: {enc_name}")
 
 
-def collect_cron_jobs():
-    save_command_output(['crontab', '-l'], 'crontab.txt')
-    save_command_output(['ls', '-lR', '/etc/cron*'], 'cron_dirs.txt')
+def collect_cron_jobs(verbose=False):
+    save_command_output(['crontab', '-l'], 'crontab.txt', verbose)
+    save_command_output(['ls', '-lR', '/etc/cron*'], 'cron_dirs.txt', verbose)
+
+
+def collect_user_artifacts(verbose=False):
+    home = os.path.expanduser("~")
+    save_file_copy(os.path.join(home, ".bash_history"), "bash_history", verbose)
+    ssh_dir = os.path.join(home, ".ssh")
+    if os.path.isdir(ssh_dir):
+        save_command_output(['ls', '-lR', ssh_dir], 'ssh_dir_listing.txt', verbose)
+        for file in os.listdir(ssh_dir):
+            file_path = os.path.join(ssh_dir, file)
+            save_file_copy(file_path, f"ssh_{file}", verbose)
 
 
 def generate_report():
     report_path = os.path.join(ARTIFACT_DIR, 'report.md')
     with open(report_path, 'w') as f:
         f.write("# Incident Response Report\n\n")
-        f.write("Generated on: {}\n\n".format(datetime.now()))
+        f.write(f"Generated on: {datetime.now()}\n\n")
         f.write("## Collected Artifacts\n")
         for root, _, files in os.walk(ARTIFACT_DIR):
             for file in files:
                 if file != 'report.md':
-                    f.write("- {}\n".format(file))
+                    f.write(f"- {file}\n")
 
 
 def main():
@@ -72,31 +96,21 @@ def main():
 
     os.makedirs(ARTIFACT_DIR, exist_ok=True)
 
-    # Collect process list
-    save_command_output(['ps', 'auxf'], 'processes.txt')
+    save_command_output(['ps', 'auxf'], 'processes.txt', args.verbose)
+    save_command_output(['netstat', '-tulpen'], 'netstat.txt', args.verbose)
+    save_command_output(['lsof'], 'lsof.txt', args.verbose)
+    save_command_output(['lsmod'], 'lsmod.txt', args.verbose)
+    save_command_output(['dmesg', '--ctime'], 'dmesg.txt', args.verbose)
 
-    # Collect network info
-    save_command_output(['netstat', '-tulpen'], 'netstat.txt')
+    save_file_copy('/etc/passwd', 'passwd', args.verbose)
+    save_file_copy('/etc/shadow', 'shadow', args.verbose)
+    save_file_copy('/etc/sudoers', 'sudoers', args.verbose)
+    save_file_copy('/var/log/auth.log', 'auth.log', args.verbose)
+    save_file_copy('/var/log/syslog', 'syslog', args.verbose)
 
-    # Collect open files
-    save_command_output(['lsof'], 'lsof.txt')
+    collect_cron_jobs(args.verbose)
+    collect_user_artifacts(args.verbose)
 
-    # Collect kernel modules and messages
-    save_command_output(['lsmod'], 'lsmod.txt')
-    save_command_output(['dmesg', '--ctime'], 'dmesg.txt')
-
-    # Copy important config files
-    save_file_copy('/etc/passwd', 'passwd')
-    save_file_copy('/etc/shadow', 'shadow')
-    save_file_copy('/etc/sudoers', 'sudoers')
-
-    # Logs
-    save_file_copy('/var/log/auth.log', 'auth.log')
-    save_file_copy('/var/log/syslog', 'syslog')
-
-    # TODO: Add more artifacts: cron jobs, bash history, etc.
-
-    # Compute hashes for all collected files
     manifest = []
     for root, _, files in os.walk(ARTIFACT_DIR):
         for name in files:
@@ -104,15 +118,11 @@ def main():
             filehash = compute_hash(path)
             manifest.append(f"{name}\t{filehash}")
 
-    # Write manifest file
     with open(os.path.join(ARTIFACT_DIR, 'manifest.txt'), 'w') as f:
         f.write('\n'.join(manifest))
 
-    # Package all artifacts into a .tar.gz archive
     if args.package:
-        package_artifacts()
-
-    collect_cron_jobs()
+        package_artifacts(encrypt=args.encrypt)
 
     generate_report()
 
